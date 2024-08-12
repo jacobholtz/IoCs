2081150768b93461c9abc4ff17bd9bc435ef70ee
Jacob Holtz <jacob.holtz20@gmail.com>
2024-08-12 13:36:51 -0500

    Move file to this directory, add code to write to file

--- hashes.py
+++ hashes.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+# Open file to write
+f = open("LockBit_sha256_hashes", "w")
+
 data = {
     'query': 'get_siginfo',
     'signature': 'lockbit',
@@ -13,4 +16,7 @@ hashDict = json.loads(json_response)
 
 for hashValue in hashDict['data']:
     hash = hashValue['sha256_hash']
-    print(hash)
+    f.write(hash)
+
+# Close the file
+f.close()
